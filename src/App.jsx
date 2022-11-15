b0b3c62178773cc630c505dc86093001aac0b64e
Sarthak Sidhant <52284479+Sarthak-Sidhant@users.noreply.github.com>
2022-11-15 05:34:36 +0530

    Initialize burkeholland-remove-start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/**
+ * Application component
+ * 
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+import Header from "./Components/Header";
+
+const App = () => {
+  return <Header/>;
+};
+
+export default App;
